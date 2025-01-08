e88c24a4941d18e4cd121eb316e569a82201d3e4
Juan <juanmunozmedina96@gmail.com>
2025-01-08 22:35:30 +0100

    Sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -4,93 +4,95 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import { defineConfig, envField } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind({ applyBaseStyles: false }), icon(), solidJs()],
-	// Update to your storefront URL
-	site: 'https://maquiavelowines.netlify.app',
-	output: 'server',
-	adapter: netlify({ imageCDN: true }),
-	vite: {
-		build: {
-			assetsInlineLimit(filePath) {
-				return filePath.endsWith('css');
-			},
-		},
-	},
-	image: {
-		// Update to your own image domains
-		domains: [
-			'localhost',
-			'maquiavelowines.com',
-			'maquiavelowines.vercel.app',
-			'maquiavelowines.netlify.app',
-		],
-	},
-	experimental: {
-		env: {
-			schema: {
-				STRIPE_SECRET_KEY: envField.string({
-					context: 'server',
-					access: 'secret',
-					// This is a random test key
-					//default: 'sk_test_CGGvfNiIPwLXiDwaOfZ3oX6Y',
-				}),
-				FATHOM_SITE_ID: envField.string({
-					context: 'client',
-					access: 'public',
-					optional: true,
-				}),
-				GOOGLE_GEOLOCATION_SERVER_KEY: envField.string({
-					context: 'server',
-					access: 'secret',
-					optional: true,
-				}),
-				GOOGLE_MAPS_BROWSER_KEY: envField.string({
-					context: 'client',
-					access: 'public',
-					optional: true,
-				}),
-				LOOPS_API_KEY: envField.string({
-					context: 'server',
-					access: 'secret',
-					optional: true,
-				}),
-				LOOPS_SHOP_TRANSACTIONAL_ID: envField.string({
-					context: 'server',
-					access: 'public',
-					optional: true,
-				}),
-				LOOPS_FULFILLMENT_TRANSACTIONAL_ID: envField.string({
-					context: 'server',
-					access: 'public',
-					optional: true,
-				}),
-				LOOPS_FULFILLMENT_EMAIL: envField.string({
-					context: 'server',
-					access: 'public',
-					optional: true,
-				}),
-				// Used by the Astro team for our internal backend
-				SHOP_API_URL: envField.string({
-					context: 'server',
-					access: 'public',
-					optional: true,
-				}),
-				SHOP_API_KEY: envField.string({
-					context: 'server',
-					access: 'secret',
-					optional: true,
-				}),
-				/*ES_SHIPPING_RATE_ID: envField.string({
-					context: 'server',
-					access: 'secret',
-				}),
-				INTERNATIONAL_SHIPPING_RATE_ID: envField.string({
-					context: 'server',
-					access: 'secret',
-				}),*/
-			},
-		},
-	},
+    integrations: [tailwind({ applyBaseStyles: false }), icon(), solidJs(), sitemap()],
+    // Update to your storefront URL
+    site: 'https://maquiavelowines.netlify.app',
+    output: 'server',
+    adapter: netlify({ imageCDN: true }),
+    vite: {
+        build: {
+            assetsInlineLimit(filePath) {
+                return filePath.endsWith('css');
+            },
+        },
+    },
+    image: {
+        // Update to your own image domains
+        domains: [
+            'localhost',
+            'maquiavelowines.com',
+            'maquiavelowines.vercel.app',
+            'maquiavelowines.netlify.app',
+        ],
+    },
+    experimental: {
+        env: {
+            schema: {
+                STRIPE_SECRET_KEY: envField.string({
+                    context: 'server',
+                    access: 'secret',
+                    // This is a random test key
+                    //default: 'sk_test_CGGvfNiIPwLXiDwaOfZ3oX6Y',
+                }),
+                FATHOM_SITE_ID: envField.string({
+                    context: 'client',
+                    access: 'public',
+                    optional: true,
+                }),
+                GOOGLE_GEOLOCATION_SERVER_KEY: envField.string({
+                    context: 'server',
+                    access: 'secret',
+                    optional: true,
+                }),
+                GOOGLE_MAPS_BROWSER_KEY: envField.string({
+                    context: 'client',
+                    access: 'public',
+                    optional: true,
+                }),
+                LOOPS_API_KEY: envField.string({
+                    context: 'server',
+                    access: 'secret',
+                    optional: true,
+                }),
+                LOOPS_SHOP_TRANSACTIONAL_ID: envField.string({
+                    context: 'server',
+                    access: 'public',
+                    optional: true,
+                }),
+                LOOPS_FULFILLMENT_TRANSACTIONAL_ID: envField.string({
+                    context: 'server',
+                    access: 'public',
+                    optional: true,
+                }),
+                LOOPS_FULFILLMENT_EMAIL: envField.string({
+                    context: 'server',
+                    access: 'public',
+                    optional: true,
+                }),
+                // Used by the Astro team for our internal backend
+                SHOP_API_URL: envField.string({
+                    context: 'server',
+                    access: 'public',
+                    optional: true,
+                }),
+                SHOP_API_KEY: envField.string({
+                    context: 'server',
+                    access: 'secret',
+                    optional: true,
+                }),
+                /*ES_SHIPPING_RATE_ID: envField.string({
+                    context: 'server',
+                    access: 'secret',
+                }),
+                INTERNATIONAL_SHIPPING_RATE_ID: envField.string({
+                    context: 'server',
+                    access: 'secret',
+                }),*/
+            },
+        },
+    },
 });
