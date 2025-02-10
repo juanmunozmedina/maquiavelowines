829f638c19783f7f90c71496fef0472c537cc16f
Juan <juanmunozmedina96@gmail.com>
2025-02-10 23:30:11 +0100

    Dark mode in cart and change prefers-color-scheme

--- src/features/cart/CartItem.tsx
+++ src/features/cart/CartItem.tsx
@@ -75,18 +75,18 @@ export function CartItem(props: { item: LineItem; class?: string }) {
 				</a>
 
 				<div class="flex flex-1 flex-col">
-					<p class="mt-auto py-2 text-lg/none font-medium text-slate-700">
+					<p class="mt-auto py-2 text-lg/none font-medium text-slate-700 dark:text-theme-base-100">
 						{props.item.productVariant.product.name}
 					</p>
 
 					<Show when={Object.values(props.item.productVariant.options).length > 0}>
-						<p class="-mt-[3px] font-medium text-slate-500">
+						<p class="-mt-[3px] font-medium text-slate-500 dark:text-theme-base-100">
 							{Object.values(props.item.productVariant.options).join(' • ')}
 						</p>
 					</Show>
 
 					{/* this translation is for visual vertical centering */}
-					<p class="-translate-y-0.5 py-2 font-medium leading-none text-slate-600">
+					<p class="-translate-y-0.5 py-2 font-medium leading-none text-slate-600 dark:text-theme-base-100">
 						<ProductPrice
 							price={
 								props.item.productVariant.product.price + props.item.productVariant.priceVariant
@@ -111,7 +111,7 @@ export function CartItem(props: { item: LineItem; class?: string }) {
 
 				<button
 					type="button"
-					class="-my-2 self-end p-2 text-slate-600 hover:text-slate-700"
+					class="-my-2 self-end p-2 text-slate-600 hover:text-slate-700 dark:text-theme-base-100 dark:hover:text-theme-base-500"
 					onClick={() => deleteMutation.mutate()}
 				>
 					<RiSystemDeleteBinLine class="size-6" />
