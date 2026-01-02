f5dc12e3d0593be8646cc8b94a087b4dd6798104
Juan <156125311+juanmunozmedina@users.noreply.github.com>
2026-01-02 14:02:06 +0100

    FIX NumberInput.tsx

--- src/components/ui/NumberInput.tsx
+++ src/components/ui/NumberInput.tsx
@@ -1,15 +1,69 @@
-import { Show, type JSX } from 'solid-js';
+import { RiSystemAddFill, RiSystemSubtractFill } from 'solid-icons/ri';
+import { type ComponentProps, type JSX, splitProps } from 'solid-js';
+import { clamp } from '~/lib/util.ts';
 
-export function NumberInput(props: NumberInputProps) {
+export function NumberInput(
+	props: JSX.InputHTMLAttributes<HTMLInputElement> & {
+		min?: number;
+		max?: number;
+		value: number;
+		setValue: (value: number) => void;
+	},
+) {
+	const min = () => props.min ?? 0;
+	const max = () => 9;
+
+	const update = (newValueInput: number) => {
+		const newValue = clamp(newValueInput, min(), max());
+		if (newValue !== props.value) {
+			props.setValue(newValue);
+		}
+	};
+
 	return (
-		<Show when={props.value > 1}>
-			<div
-				{...props}
-				class={`flex w-fit text-slate-600 dark:text-theme-base-100 ${props.class ?? ''}`}
+		<div class="invisible flex h-11 w-fit items-stretch divide-x divide-slate-300 border border-slate-300 bg-slate-100 text-slate-600 dark:border-slate-600 dark:bg-slate-700 dark:text-theme-base-100">
+			<NumberInputButton
+				icon={<RiSystemSubtractFill />}
+				onClick={() => update(props.value - 1)}
+				disabled={props.disabled ?? props.value <= min()}
 			>
-				<span class="font-medium">Cantidad:</span>
-				<span class="ml-2">{props.value}</span>
-			</div>
-		</Show>
+				Decremento
+			</NumberInputButton>
+			<input
+				{...props}
+				min={min()}
+				max={max()}
+				type="number"
+				readOnly
+				class="w-12 bg-white p-2 text-center [appearance:textfield] dark:bg-slate-700 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+				value={props.value}
+				onInput={(e) => update(e.currentTarget.valueAsNumber)}
+			/>
+			<NumberInputButton
+				icon={<RiSystemAddFill />}
+				onClick={() => update(props.value + 1)}
+				disabled={props.disabled ?? props.value >= max()}
+			>
+				Incremento
+			</NumberInputButton>
+		</div>
+	);
+}
+
+function NumberInputButton(
+	props: ComponentProps<'button'> & {
+		icon: JSX.Element;
+	},
+) {
+	const [local, others] = splitProps(props, ['icon', 'children']);
+	return (
+		<button
+			type="button"
+			class="flex aspect-square h-full items-center justify-center transition-colors hover:bg-slate-200 disabled:cursor-not-allowed disabled:text-slate-400 dark:hover:bg-theme-base-900"
+			{...others}
+		>
+			<span class="sr-only">{local.children}</span>
+			{local.icon}
+		</button>
 	);
 }
