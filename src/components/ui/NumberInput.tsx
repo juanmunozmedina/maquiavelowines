2895bb17166db7966570b8f2b4b12f81e0788ccb
Juan <156125311+juanmunozmedina@users.noreply.github.com>
2026-01-02 13:50:33 +0100

    Update NumberInput.tsx

--- src/components/ui/NumberInput.tsx
+++ src/components/ui/NumberInput.tsx
@@ -1,17 +1,15 @@
-import type { JSX } from 'solid-js';
-
-type NumberInputProps = {
-	value: number;
-} & JSX.HTMLAttributes<HTMLDivElement>;
+import { Show, type JSX } from 'solid-js';
 
 export function NumberInput(props: NumberInputProps) {
 	return (
-		<div
-			{...props}
-			class={`flex w-fit text-slate-600 dark:text-theme-base-100 ${props.class ?? ''}`}
-		>
-			<span class="font-medium">Cantidad:</span>
-			<span class="ml-2">{props.value}</span>
-		</div>
+		<Show when={props.value > 1}>
+			<div
+				{...props}
+				class={`flex w-fit text-slate-600 dark:text-theme-base-100 ${props.class ?? ''}`}
+			>
+				<span class="font-medium">Cantidad:</span>
+				<span class="ml-2">{props.value}</span>
+			</div>
+		</Show>
 	);
 }
